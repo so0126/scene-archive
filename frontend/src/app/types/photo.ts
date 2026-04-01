c944290e040b5c2de0cb27693c6a7a44befb2776
so0126 <nada981226@gmail.com>
2026-04-01 19:13:43 +0900

    front/chore: 피그마 메이크로 기본 페이지 만듬

--- frontend/src/app/types/photo.ts
+++ frontend/src/app/types/photo.ts
@@ -1,9 +1,7 @@
-export type PhotoStatus = "uploading" | "processing" | "ready" | "completed";
-
 export interface PhotoData {
   id: string;
   image: string;
   keywords: string;
-  status: PhotoStatus;
-  essay?: string;
+  processing: boolean;
+  processed: boolean;
 }
